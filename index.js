390e2fd6dc9ae8e00c8555053b77629bc7b15174
BrunoCostaPrado <brunocostaprado@gmail.com>
2021-10-29 09:18:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const express = require("express");
 const app = express();
+
+app.get('/api', (req, resp )=>{
+const apiKey = req.query.apiKey;
+resp.send({ "data":"Teste"});
+});
+app.listen(8080, ()=> console.log('vivo em http://localhost:8080'));
